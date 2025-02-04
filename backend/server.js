27d91f9536394564a2d314895283d6bc966582d2
AdieDev0 <devutella@gmail.com>
2025-02-04 17:10:41 +0800

    change colors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const colors = require("colors/safe");
 
 dotenv.config();
 const app = express();
@@ -12,8 +13,8 @@ app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
+  .then(() => console.log(colors.rainbow("MongoDB Connected")))
   .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(colors.rainbow(`Server running on port ${PORT}`)));
