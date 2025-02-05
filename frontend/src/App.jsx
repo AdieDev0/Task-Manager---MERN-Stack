a5247b847e2b988965aaf2090b0359e252d0e1aa
AdieDev0 <devutella@gmail.com>
2025-02-05 14:43:00 +0800

    App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import TaskList from "./pages/TaskList";
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<TaskList />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
