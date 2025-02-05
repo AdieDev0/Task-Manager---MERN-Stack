868e1aefd319e917153c8415f6dfbb64363d65ad
AdieDev0 <devutella@gmail.com>
2025-02-05 18:54:57 +0800

    Update

--- frontend/src/api/apiBackend.js
+++ frontend/src/api/apiBackend.js
@@ -2,18 +2,18 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: "http://localhost:5000/api" });
 
-// Attach token to requests
+// TOKEN REQUEST
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
 });
 
-// **Authentication APIs**
+// AUTHENTICATION API
 export const registerUser = (userData) => API.post("/auth/register", userData);
 export const loginUser = (userData) => API.post("/auth/login", userData);
 
-// **Task APIs**
+// TASK API
 export const fetchTasks = () => API.get("/tasks");
 export const createTask = (taskData) => API.post("/tasks", taskData);
 export const updateTask = (id, taskData) => API.put(`/tasks/${id}`, taskData);
