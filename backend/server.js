9dccba470f5d66f1c7bbaedf4aef1d19cabdf36c
AdieDev0 <devutella@gmail.com>
2025-02-07 23:11:21 +0800

    Change console log colors for MongoDB connection and server startup

--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,10 @@ app.use("/api/tasks", require("./routes/task"));
 // DATABASE CONNECTION
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log(colors.green("MongoDB Connected")))
+  .then(() => console.log(colors.rainbow("MongoDB Connected")))
   .catch((err) => console.error(colors.red(err)));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
-  console.log(colors.blue(`Server running on port ${PORT}`))
+  console.log(colors.rainbow(`Server running on port ${PORT}`))
 );
